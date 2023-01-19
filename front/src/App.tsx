0ed64edb70167c1d6793e7488a59c36cb505b513
hichemCodes <laouarmedhichem23@gmail.com>
2023-01-19 15:18:25 +0100

    add routes

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,24 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Routes, Route, useNavigate } from "react-router-dom";
+import Home from "./Components/Home";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+          <Route path="/" element={<Home />} />
+      </Routes>
+       
     </div>
   );
 }
---
+++ front/src/Components/Card.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+
+const Card = () => {
+    return (
+        <div>Card </div>
+    )
+}
+
+export default Card
