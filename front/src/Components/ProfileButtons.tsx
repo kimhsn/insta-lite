fdf311fc6ac3c25934e821720401862437acbb49
Hakim <hakimhsn06@gmail.com>
2023-01-20 06:09:39 +0100

    feat: added user profile page

---
+++ front/src/Components/ProfileButtons.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import styled from "styled-components";
+
+const ButtonWrapper = styled.div`
+  padding: 25px 20px;
+`;
+
+const ButtonRow = styled.div`
+  display: flex;
+  :not(:last-child) {
+    margin-bottom: 8px;
+  }
+`;
+
+const Button = styled.button`
+  background-color: transparent;
+  color: white;
+  border: 1px solid #919191;
+  padding: 12px 0;
+  cursor: pointer;
+  width: 100%;
+  border-radius: 5px;
+  font-size: 14px;
+  :not(:last-child) {
+    margin-right: 8px;
+  }
+`;
+
+function ProfileButtons() {
+  return (
+    <ButtonWrapper>
+      <ButtonRow>
+        <Button>Editer mon profil</Button>
+        <Button>Ajouter une image/video</Button>
+      </ButtonRow>
+    </ButtonWrapper>
+  );
+}
+
+export default ProfileButtons;
