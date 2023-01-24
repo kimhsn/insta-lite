d1be618cc5aca892e65c1c9ecd867fc0ba07c586
Hakim <hakimhsn06@gmail.com>
2023-01-24 21:00:07 +0100

    fix file-saver bug

--- front/src/components/UsersManagement/ReadOnlyRowUser.tsx
+++ front/src/components/UsersManagement/ReadOnlyRowUser.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { AiFillDelete } from "react-icons/ai";
 import { FaEdit } from "react-icons/fa";
+import scLogo from "../../assets/scLogo.jpg";
 
 type User = {
   id: number;
   nom: string;
   prenom: string;
   email: string;
+  creationData: string;
   appRoles: {
     roleName: string;
   };
@@ -24,11 +26,7 @@ const ReadOnlyRowUser = (props: Props) => {
       <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
         <div className="flex">
           <div className="flex-shrink-0 w-10 h-10">
-            <img
-              className="w-full h-full rounded-full"
-              src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.2&w=160&h=160&q=80"
-              alt=""
-            />
+            <img className="w-full h-full rounded-full" src={scLogo} alt="" />
           </div>
           <div className="ml-3">
             <p className="text-gray-900 whitespace-no-wrap">
@@ -39,7 +37,7 @@ const ReadOnlyRowUser = (props: Props) => {
         </div>
       </td>
       <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
-        <p className="text-gray-900 whitespace-no-wrap">16 Dec 29</p>
+        <p className="text-gray-900 whitespace-no-wrap">{user.creationData}</p>
       </td>
       <td className="px-5 py-5 border-b border-gray-200 bg-white text-sm">
         {user.appRoles.roleName === "ADMIN" ? (
